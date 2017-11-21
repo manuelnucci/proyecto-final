02dacf9b534cf26c476722c8b58dd33ae612abf7
Manuel Nucci <manuucci96@gmail.com>
2017-11-21 17:29:30 -0300

    Corrección del escenario en la clase UbicarProfesorTest.

--- PGA/src/tests/UbicarProfesorTest.java
+++ PGA/src/tests/UbicarProfesorTest.java
@@ -24,7 +24,7 @@ public class UbicarProfesorTest
     @Before
     public void setUp() throws Exception
     {
-        fixture.setUpAgregarYModif();
+        fixture.setUpUbicar();
     }
 
     @After
